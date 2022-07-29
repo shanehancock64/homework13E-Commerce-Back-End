ebfef53e776345405c6e8e7ed9faeb5d9dfa9398
Michael Hancock <shanehancock64@live.com>
2022-07-29 14:47:42 -0700

    Update category-routes.js
    put route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -88,6 +88,43 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Product.update(req.body, {
+    where: {
+        id: req.params.id,
+    },
+})
+.then((product) => {
+
+    return ProductTag.findAll({ where: { product_id: req.params.id } });
+})
+.then((productTags) => {
+
+    const productTagIds = productTags.map(({ tag_id }) => tag_id);
+
+    const newProductTags = req.body.tagIds
+        .filter((tag_id) => !productTagIds.includes(tag_id))
+        .map((tag_id) => {
+            return {
+                product_id: req.params.id,
+                tag_id,
+            };
+        });
+
+    const productTagsToRemove = productTags
+        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .map(({ id }) => id);
+
+
+    return Promise.all([
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
+    ]);
+})
+.then((updatedProductTags) => res.json(updatedProductTags))
+.catch((err) => {
+
+    res.status(400).json(err);
+});
 });
 
 router.delete('/:id', (req, res) => {
