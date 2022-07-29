518464b4e69062644f96bcb1eff036369c0e2a99
Michael Hancock <shanehancock64@live.com>
2022-07-29 14:47:18 -0700

    Update category-routes.js
    Post route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -58,6 +58,32 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds
+})
+.then((product) => {
+
+    if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+            return {
+                product_id: product.id,
+                tag_id,
+            };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+    }
+
+    res.status(200).json(product);
+})
+.then((productTagIds) => res.status(200).json(productTagIds))
+.catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+});
 });
 
 router.put('/:id', (req, res) => {
