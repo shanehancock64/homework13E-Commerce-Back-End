6b45c5f467c17d5d7d874396514c0e5c0ca57869
Michael Hancock <shanehancock64@live.com>
2022-07-29 14:46:03 -0700

    find all route
    product routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,23 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+            model: Category,
+            attributes: ['category_name']
+        },
+        {
+            model: Tag,
+            attributes: ['tag_name']
+        }
+    ]
+})
+.then(dbProductData => res.json(dbProductData))
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
 
 router.get('/:id', (req, res) => {
