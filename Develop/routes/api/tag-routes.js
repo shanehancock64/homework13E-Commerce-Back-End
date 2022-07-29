9a01d7ddac31497e91e34ca4876bd136f2a82462
Michael Hancock <shanehancock64@live.com>
2022-07-29 14:56:20 -0700

    Update tag-routes.js
    find one*

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -22,6 +22,20 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+        id: req.params.id
+    },
+    include: {
+        model: Product,
+        attributes: ['product_name', 'price', 'stock', 'category_id']
+    }
+})
+.then(dbTagData => res.json(dbTagData))
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
 
 router.post('/', (req, res) => {
