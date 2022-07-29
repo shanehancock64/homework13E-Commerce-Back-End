99354a5c1dd1fdf6a0ca74342c85e662658f2c01
Michael Hancock <shanehancock64@live.com>
2022-07-29 14:50:23 -0700

    Update product-routes.js
    Create Product

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -67,7 +67,33 @@ router.post('/', (req, res) => {
       stock: 3,
       tagIds: [1, 2, 3, 4]
     }
-  */
+  */Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds
+})
+.then((product) => {
+
+    if (req.body.tagIds.length) {
+        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+            return {
+                product_id: product.id,
+                tag_id,
+            };
+        });
+        return ProductTag.bulkCreate(productTagIdArr);
+    }
+
+    res.status(200).json(product);
+})
+.then((productTagIds) => res.status(200).json(productTagIds))
+.catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+});
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
