7559f38d0dffdbf0d0a2e98d90ae47d07f32ba5b
Michael Hancock <shanehancock64@live.com>
2022-07-29 14:46:44 -0700

    Update category-routes.js
    created find one route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -28,6 +28,32 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Product.findOne({
+    where: {
+        id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+            model: Category,
+            attributes: ['category_name']
+        },
+        {
+            model: Tag,
+            attributes: ['tag_name']
+        }
+    ]
+})
+.then(dbProductData => {
+    if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+    }
+    res.json(dbProductData);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
 
 router.post('/', (req, res) => {
