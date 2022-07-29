39a8d2aef45a47d412b4a0f144a3d004055968ad
Michael Hancock <shanehancock64@live.com>
2022-07-29 14:42:07 -0700

    Update server.js
    server finished

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
