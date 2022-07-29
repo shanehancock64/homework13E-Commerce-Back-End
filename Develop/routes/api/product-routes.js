1b6da16138dc901e24326d55b81feff432d09704
Michael Hancock <shanehancock64@live.com>
2022-07-29 14:49:26 -0700

    Update product-routes.js
    get one

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -30,6 +30,32 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+        id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+            model: Category,
+            attributes: ['category_name']
+        },
+        {
+            model: Tag,
+            attributes: ['tag_name']
+        }
+    ]
+})
+.then(dbProductData => {
+    if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+    }
+    res.json(dbProductData);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
 });
 
 // create new product
